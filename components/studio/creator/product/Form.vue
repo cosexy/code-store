4c390bf7bbf430caa238e1ea67f7aa4ae30b8d80
Guen <heloo@guen.dev>
2023-05-10 15:15:51 +0700

    update form

--- components/studio/creator/product/Form.vue
+++ components/studio/creator/product/Form.vue
@@ -273,15 +273,6 @@
         </form-item>
       </studio-creator-product-section>
     </div>
-
-    <div class="mt-6 flex items-center justify-end gap-x-6">
-      <button type="button" class="text-sm font-semibold leading-6 text-white">
-        Cancel
-      </button>
-      <button type="submit" class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
-        Save
-      </button>
-    </div>
   </form-instance>
 </template>
 
--- pages/studio/creator/product/index.vue
+++ pages/studio/creator/product/index.vue
@@ -2,10 +2,26 @@
   <div>
     <includes-spinner :spinning="loading">
       <studio-creator-product-form
+        id="form-studio"
         v-model:form="input"
         @on-submit="submitForm($event)"
       />
     </includes-spinner>
+
+    <includes-teleport to="#actions">
+      <button
+        form="form-studio"
+        type="submit"
+        class="inline-flex items-center gap-x-1.5 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        <icon
+          :name="loading ? 'line-md:loading-twotone-loop' : 'material-symbols:add-rounded'"
+          class="-ml-0.5 h-5 w-5"
+          aria-hidden="true"
+        />
+        Create
+      </button>
+    </includes-teleport>
   </div>
 </template>
 
