9c50f9d3d8d0912ab37735e518d1836dd4da9c4c
Guen <heloo@guen.dev>
2023-05-12 00:21:54 +0700

    semi-support review

--- apollo/clients/default.ts
+++ apollo/clients/default.ts
@@ -1,8 +1,21 @@
 // eslint-disable-next-line import/named
 import { defineApolloClient } from '@nuxtjs/apollo'
+import { offsetLimitPagination } from '@apollo/client/utilities/policies/pagination'
 
 export default defineApolloClient({
   httpEndpoint: process.env.NUXT_PUBLIC_APOLLO_ENDPOIT!,
   wsEndpoint: process.env.NUXT_PUBLIC_APOLLO_WS_ENDPOIT,
-  tokenName: 'apollo:codestore.token'
+  tokenName: 'apollo:codestore.token',
+  inMemoryCacheOptions: {
+    typePolicies: {
+      Query: {
+        fields: {
+          reviews: {
+            ...offsetLimitPagination(['filter', ['sort', 'product', 'user']])
+          }
+        }
+      }
+    }
+  },
+  connectToDevTools: true
 })
