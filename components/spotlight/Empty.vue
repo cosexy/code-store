6582d67cbc65ad5d9bd04b9b2556cb996c7d0f47
Guen <heloo@guen.dev>
2023-05-12 12:11:33 +0700

    add auto scroll

---
+++ components/spotlight/Empty.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="relative flex flex-col items-center px-6 py-14 text-center text-sm sm:px-14"
+  >
+    <div class="z-10 -my-14 overflow-hidden">
+      <vue-lottie-player
+        path="https://assets9.lottiefiles.com/packages/lf20_EMTsq1.json"
+        loop
+        width="300px"
+        height="300px"
+      />
+    </div>
+
+    <p class="relative z-20 mt-2 text-gray-500">
+      Nothing here. I am sure you will find something interesting.
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import { VueLottiePlayer } from '@nguyenshort/vue-lottie'
+</script>
+
+<style scoped>
+
+</style>
--- components/spotlight/Search.vue
+++ components/spotlight/Search.vue
@@ -6,77 +6,85 @@
     hold
   >
     <includes-spinner :spinning="loading">
-      <div v-auto-animate class="flex divide-x divide-gray-100">
+      <div class="flex divide-x divide-gray-100">
         <div
+          v-if="!isEmpty"
           v-auto-animate
           :class="['max-h-96 min-w-0 flex-auto scroll-py-4 overflow-y-auto px-6 py-4', activeOption && 'sm:h-96']"
         >
-          <h2
-            class="mb-4 mt-2 text-xs font-semibold text-gray-500"
-          >
-            Content creators
-          </h2>
-          <div class="-mx-2 text-sm text-gray-700">
-            <headless-combobox-option
-              v-for="user in users"
-              :key="user.id"
-              v-slot="{ active }"
-              :value="user"
-              as="template"
+          <template v-if="users.length">
+            <h2
+              class="mb-4 mt-2 text-xs font-semibold text-gray-500"
             >
-              <div
-                :class="['group flex cursor-default select-none items-center rounded-md p-2', active && 'bg-gray-100 text-gray-900']"
+              Content creators
+            </h2>
+            <div class="-mx-2 text-sm text-gray-700">
+              <headless-combobox-option
+                v-for="user in users"
+                :key="user.id"
+                v-slot="{ active }"
+                :value="user"
+                as="template"
               >
-                <nuxt-img
-                  :src="user.avatar?.path || '/images/user.png'"
-                  :provider="user.avatar?.path ? 'backend' : ''"
-                  class="h-6 w-6 flex-none rounded-full"
-                />
+                <div
+                  :class="['group flex cursor-default select-none items-center rounded-md p-2', active && 'bg-gray-100 text-gray-900']"
+                >
+                  <nuxt-img
+                    :src="user.avatar?.path || '/images/user.png'"
+                    :provider="user.avatar?.path ? 'backend' : ''"
+                    class="h-6 w-6 flex-none rounded-full"
+                  />
 
-                <span class="ml-3 flex-auto truncate">{{ user.name }}</span>
-                <icon
-                  v-if="active"
-                  name="material-symbols:chevron-right-rounded"
-                  class="ml-3 h-5 w-5 flex-none text-gray-400"
-                  aria-hidden="true"
-                />
-              </div>
-            </headless-combobox-option>
-          </div>
+                  <span class="ml-3 flex-auto truncate">{{ user.name }}</span>
+                  <icon
+                    v-if="active"
+                    name="material-symbols:chevron-right-rounded"
+                    class="ml-3 h-5 w-5 flex-none text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+              </headless-combobox-option>
+            </div>
+          </template>
 
-          <h2
-            class="mb-4 mt-6 text-xs font-semibold text-gray-500"
-          >
-            Products
-          </h2>
-          <div class="-mx-2 text-sm text-gray-700">
-            <headless-combobox-option
-              v-for="product in products"
-              :key="product.id"
-              v-slot="{ active }"
-              :value="product"
-              as="template"
+          <template v-if="products.length">
+            <h2
+              class="mb-4 text-xs font-semibold text-gray-500"
+              :class="[users.length && 'mt-6']"
             >
-              <div
-                :class="['group flex cursor-default select-none items-center rounded-md p-2', active && 'bg-gray-100 text-gray-900']"
+              Products
+            </h2>
+            <div class="-mx-2 text-sm text-gray-700">
+              <headless-combobox-option
+                v-for="product in products"
+                :key="product.id"
+                v-slot="{ active }"
+                :value="product"
+                as="template"
               >
-                <nuxt-img
-                  :src="product.avatar!.path"
-                  provider="backend"
-                  class="aspect-product h-6 flex-none rounded-md"
-                />
+                <div
+                  :class="['group flex cursor-default select-none items-center rounded-md p-2', active && 'bg-gray-100 text-gray-900']"
+                >
+                  <nuxt-img
+                    :src="product.avatar!.path"
+                    provider="backend"
+                    class="aspect-product h-6 flex-none rounded-md"
+                  />
 
-                <span class="ml-3 line-clamp-1 flex-auto truncate">{{ product.name }}</span>
-                <icon
-                  v-if="active"
-                  name="material-symbols:chevron-right-rounded"
-                  class="ml-3 h-5 w-5 flex-none text-gray-400"
-                  aria-hidden="true"
-                />
-              </div>
-            </headless-combobox-option>
-          </div>
+                  <span class="ml-3 line-clamp-1 flex-auto truncate">{{ product.name }}</span>
+                  <icon
+                    v-if="active"
+                    name="material-symbols:chevron-right-rounded"
+                    class="ml-3 h-5 w-5 flex-none text-gray-400"
+                    aria-hidden="true"
+                  />
+                </div>
+              </headless-combobox-option>
+            </div>
+          </template>
         </div>
+
+        <spotlight-empty v-else class="w-full" />
 
         <component
           :is="activeOption.__typename === 'User' ? SpotlightUser : SpotlightProduct"
@@ -120,6 +128,8 @@ const products = computed<SpotlightQuery['products']>(() => result.value?.produc
 const { result, loading } = useQuery(SpotlightDocument, filter, {
   debounce: 500
 })
+
+const isEmpty = computed(() => !loading.value && !users.value.length && !products.value.length)
 </script>
 
 <style scoped>
