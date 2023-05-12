9dfb2bb0a94afa811356b21d111f02f4c67400fd
Guen <heloo@guen.dev>
2023-05-12 14:28:19 +0700

    add go to search

--- components/search/Result.vue
+++ components/search/Result.vue
@@ -16,10 +16,32 @@
 
       <includes-pagination :total="count" />
     </template>
+
+    <div v-else-if="loading" class="flex flex-col items-center justify-center">
+      <vue-lottie-player
+        width="250px"
+        height="200px"
+        loop
+        path="/json/loading.json"
+      />
+    </div>
+
+    <div v-else class="flex flex-col items-center justify-center">
+      <vue-lottie-player
+        path="https://assets9.lottiefiles.com/packages/lf20_EMTsq1.json"
+        loop
+        width="300px"
+        height="300px"
+      />
+      <p class="relative z-20 -mt-2 text-sm text-gray-500">
+        Nothing here. I am sure you will find something interesting.
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { VueLottiePlayer } from '@nguyenshort/vue-lottie'
 import {
   ProductsCountQueryVariables,
   SearchProductsDocument,
@@ -37,7 +59,7 @@ const emit = defineEmits<{
 
 const vars = useVModel(props, 'filter', emit)
 
-const { result } = useQuery(SearchProductsDocument, vars, {
+const { result, loading } = useQuery(SearchProductsDocument, vars, {
   debounce: 500
 })
 const products = computed<SearchProductsQuery['products']>(() => result.value?.products ?? [])
